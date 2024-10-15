99e2f34db94891804d4e604a7da6466dfc067b71
Afseen Salam <afseenfazil@gmail.com>
2024-10-15 12:51:46 -0400

    Added messaging feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { BackendService } from './services/backend.service';
 import { Item } from './models/item';
 import { GoogleSigninButtonModule, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { User } from './models/user';
+import { JoinRoomComponent } from './components/join-room/join-room.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, GoogleSigninButtonModule],
+  imports: [RouterOutlet, FormsModule, GoogleSigninButtonModule,JoinRoomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
